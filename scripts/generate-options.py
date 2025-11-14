d9be5bccc20af67157703150c097476efd59a787
excuseme <excuseme@example.com>
2025-11-14 18:09:38 +0100

    First commit

--- scripts/generate-options.py
+++ scripts/generate-options.py
@@ -3,6 +3,10 @@ import yaml
 
 URL = "https://comiccaster.xyz/comics_list.json"
 URL_POLICAL ="https://comiccaster.xyz/political_comics_list.json"
+def is_other_language(name: str):
+    if "en Español" in name:
+        return True
+    return False
 
 def get_data(url: str):
     print("Fetching:", url)
@@ -14,19 +18,23 @@ def get_data(url: str):
     options = []
     slugs = []
     print("\n# --- YAML OPTIONS ---")
-
+    comics_other_languages = []
     for c in comics:
         name = c.get("name", "Unknown")
         slug = c.get("slug", "unknown")
+        if is_other_language(name):
+            comics_other_languages.append(c)
+        else:
+            print(f"  - \"{name}\": {slug}")
+
+
+    print("\n# --- OTHER LANGUAGES YAML OPTIONS ---")
+    for c in comics_other_languages:
+        name = c.get("name", "Unknown")
+        slug = c.get("slug", "unknown")
         print(f"  - \"{name}\": {slug}")
-        slugs.append(slug)
-
-
-    print("\n# --- SLUGS COMMA ---")
-    print(",".join(slugs))
-
 
 
 if __name__ == "__main__":
-    #get_data(URL)
-    get_data(URL_POLICAL)
+    get_data(URL)
+    #get_data(URL_POLICAL)
